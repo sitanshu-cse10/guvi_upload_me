f981f8177697d01a4525e70ef6832f772e48ce3f
Sitanshu-cse10 <sitstripathi10@gmail.com>
2018-10-09 10:56:41 +0530

    positive negative program

---
+++ positive_and_negative.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    cin>>number;
+    if(number>0)
+    {
+        cout<<"Posiive";
+    }
+    else if(number==0)
+    {
+        cout<<"Zero";
+    }
+    else
+    {
+        cout<<"Negative";
+    }
+}
